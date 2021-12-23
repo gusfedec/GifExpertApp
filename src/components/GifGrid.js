30082eaff5ce93c3c64a67360db0c31ab46387b2
gusfedec <gus.fe.c@gmail.com>
2021-12-23 08:56:33 -0300

    useEffect

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const GifGrid = ( {category} ) => {
+
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        getGifs();
+    }, [])
     
     const getGifs = async() => {
         const url = 'https://api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limit=10&api_key=lXUEyM1ClaSgRnkt9Zi8s3kWOLwnGJPl';
@@ -17,11 +23,12 @@ const GifGrid = ( {category} ) => {
         console.log(gifs);
     }
 
-    getGifs();
+    //getGifs();
 
     return (
         <div>
-            <h3>{category}</h3>
+            <h3 onClick={() => setCount(count+1) }>{category}</h3>
+            <h3>{count}</h3>
         </div>
     )
 }
