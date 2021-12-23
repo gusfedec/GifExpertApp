15fba4cc7f043272e76511eaa7e8bbd90825b5a9
gusfedec <gus.fe.c@gmail.com>
2021-12-23 10:56:11 -0300

    Mostrar los títulos de las imágenes

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,34 +1,41 @@
-import React, {useState, useEffect} from 'react'
+import React, { useEffect, useState } from 'react'
+import GifGridItem from './GifGridItem';
 
-const GifGrid = ( {category} ) => {
+const GifGrid = ({ category }) => {
 
-    const [count, setCount] = useState(0);
+    const [images, setImages] = useState([]);
 
     useEffect(() => {
         getGifs();
     }, [])
-    
-    const getGifs = async() => {
+
+    const getGifs = async () => {
         const url = 'https://api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limit=10&api_key=lXUEyM1ClaSgRnkt9Zi8s3kWOLwnGJPl';
         const resp = await fetch(url);
-        const {data} = await resp.json();
+        const { data } = await resp.json();
 
-        const gifs = data.map( img => {
+        const gifs = data.map(img => {
             return {
                 id: img.id,
                 title: img.title,
-                url:img.images?.downsized_medium.url
+                url: img.images?.downsized_medium.url
             }
-        } )
+        })
         console.log(gifs);
+        setImages(gifs);
     }
 
-    //getGifs();
+
 
     return (
         <div>
-            <h3 onClick={() => setCount(count+1) }>{category}</h3>
-            <h3>{count}</h3>
+            <h3>{category}</h3>
+                {
+                    images.map( img => {
+                        return <GifGridItem key={img.id} { ...img } />
+                    })
+                }
+
         </div>
     )
 }
